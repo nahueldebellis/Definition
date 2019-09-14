52a593c566c1431cf083252295e2a1c689d3f4b9
nahuel <debellisnahuel@gmail.com>
2019-09-13 21:26:34 -0300

    change uri and use class word

--- api/route.py
+++ api/route.py
@@ -1,22 +1,26 @@
+from word import Word
 from flask import Flask, request
 from flask import render_template as rt
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def index():
-    rt('index.html')
+	return rt("index.html")
 
-@app.route("/synonym/<word>")
+@app.route("/word/<word>")
 def synonym(word):
-    return word
+	w = Word()
+	return w.create_response(word)
+	
 
 @app.route('/noun', methods=['POST'])
 def noun():
     request.form['text']
     return text
 
-
+if __name__ == '__main__':
+    app.run()
 
 
 
